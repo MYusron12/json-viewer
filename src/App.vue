365cf842087c3b7e921e98ee9de6faee29579c20
supardiputra <93508603+pardiputram@users.noreply.github.com>
2024-04-26 21:15:11 +0700

    change

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div>
     <nav class="navbar bg-body-tertiary">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <a class="navbar-brand" href="#">Home</a>
       </div>
     </nav>
     <div class="container-fluid">
