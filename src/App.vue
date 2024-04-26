f1d640f6aadade04439a2d8c89ea287e63ae6059
supardiputra <93508603+pardiputram@users.noreply.github.com>
2024-04-26 20:58:58 +0700

    first push

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <nav class="navbar bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+      </div>
+    </nav>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-5">
+          <div class="mb-3">
+            <label for="inputJson" class="form-label">Input JSON</label>
+            <textarea v-model="inputJson" class="form-control h-100" id="inputJson" rows="40"></textarea>
+          </div>
+        </div>
+        <div class="col-lg-1 d-flex align-items-center justify-content-center">
+          <button @click="formatJson" class="btn btn-primary">Format JSON</button>
+        </div>
+        <div class="col-lg-6">
+          <div class="mb-3">
+            <label for="outputJson" class="form-label">Formatted JSON</label>
+            <textarea :value="formattedJson" class="form-control h-100" id="outputJson" rows="40" readonly></textarea>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      inputJson: '',
+      formattedJson: '',
+    };
+  },
+  methods: {
+    formatJson() {
+      try {
+        const parsedJson = JSON.parse(this.inputJson);
+        this.formattedJson = JSON.stringify(parsedJson, null, 2);
+      } catch (error) {
+        console.error('Invalid JSON format:', error.message);
+        this.formattedJson = 'Invalid JSON format';
+      }
+    },
+  },
+};
+</script>
